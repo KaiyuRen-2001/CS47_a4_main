ce239911672cb00a45afe86a756d58c87660fc52
KaiyuRen-2001 <85300992+KaiyuRen-2001@users.noreply.github.com>
2023-02-24 22:53:49 -0800

    hi

---
+++ App.js
@@ -0,0 +1,39 @@
+import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { useSpotifyAuth } from "./utils";
+import { Themes } from "./assets/Themes";
+import SpotifyAuthButton from "./components/SpotifyAuthButton";
+import SongList from "./components/SongList";
+// import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import 'react-native-gesture-handler';
+
+export default function App() {
+  // Pass in true to useSpotifyAuth to use the album ID (in env.js) instead of top tracks
+  const { token, tracks, getSpotifyAuth } = useSpotifyAuth();
+
+  console.log("tracks", tracks);
+  console.log("token", token);
+
+  let contentDisplayed = null; // let because will get reassigned
+
+  if (token){
+    contentDisplayed = <SongList tracks={tracks} />
+  } else{
+    contentDisplayed = <SpotifyAuthButton authenticationFunction={getSpotifyAuth} />;
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {contentDisplayed}      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Themes.colors.background,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  
+});
---
+++ components/SongList.js
@@ -0,0 +1,54 @@
+import {
+  FlatList,
+  Text,
+  ScrollView,
+  SafeAreaView,
+  Dimensions,
+  StyleSheet,
+  View,
+  Image,
+} from "react-native";
+import Song from "./Song";
+import { Themes } from "../assets/Themes";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const renderItem = ({ item, index }) => (
+  //console.log(item)
+  <Song
+    id={index + 1}
+    image={item.imageUrl}
+    title={item.songTitle}
+    album={item.albumName}
+    artist={item.songArtists[0].name}
+    duration={item.duration}
+  />
+);
+
+const SongList = ({ tracks }) => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatList
+          data={tracks}
+          renderItem={(item) => renderItem(item)}
+          keyExtractor={(item) => item.id}
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  /*
+  topBar: {
+    display: "flex",
+    flexDirection: "row",
+    height: windowHeight * 0.05,
+    width: "auto",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  */
+});
+export default SongList;
